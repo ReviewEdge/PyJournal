bedc2f12f8145559b57eca36ce5386d1fc1c5405
Logan Richardson <reviewedge56@gmail.com>
2020-06-23 14:30:08 -0400

    Create config_example.py

---
+++ docs/config_example.py
@@ -0,0 +1,5 @@
+# Make sure you set my_project_path to equal your project's path
+# When you create your config file, make sure it is moved to the project's directory
+
+# make sure to us r in front of the string so it is interpreted as a raw string (so you don't have issues with slashes)
+my_project_path = r"C:\Users\Logan\PycharmProjects\PyJournal"
